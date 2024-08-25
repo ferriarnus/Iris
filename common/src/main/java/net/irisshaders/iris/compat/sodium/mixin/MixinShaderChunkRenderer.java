7e3ac3e19c83508ff1e6cf07fcc2f8ca6ddcb933
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2024-08-25 14:07:44 +0200

    Update MixinShaderChunkRenderer.java

--- common/src/main/java/net/irisshaders/iris/compat/sodium/mixin/MixinShaderChunkRenderer.java
+++ common/src/main/java/net/irisshaders/iris/compat/sodium/mixin/MixinShaderChunkRenderer.java
@@ -3,6 +3,8 @@ package net.irisshaders.iris.compat.sodium.mixin;
 import net.irisshaders.iris.Iris;
 import net.irisshaders.iris.pipeline.IrisRenderingPipeline;
 import net.irisshaders.iris.pipeline.WorldRenderingPipeline;
+import net.irisshaders.iris.pipeline.programs.SodiumPrograms;
+import net.irisshaders.iris.pipeline.programs.SodiumShader;
 import org.embeddedt.embeddium.impl.gl.shader.GlProgram;
 import org.embeddedt.embeddium.impl.render.chunk.ShaderChunkRenderer;
 import org.embeddedt.embeddium.impl.render.chunk.shader.ChunkShaderInterface;
@@ -11,12 +13,17 @@ import org.embeddedt.embeddium.impl.render.chunk.terrain.TerrainRenderPass;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ShaderChunkRenderer.class, remap = false)
 public abstract class MixinShaderChunkRenderer {
 	@Shadow
 	protected abstract GlProgram<ChunkShaderInterface> compileProgram(ChunkShaderOptions options);
+
+	@Shadow
+	protected GlProgram<ChunkShaderInterface> activeProgram;
 
 	@Redirect(method = "begin", at = @At(value = "INVOKE", target = "Lorg/embeddedt/embeddium/impl/render/chunk/ShaderChunkRenderer;compileProgram(Lorg/embeddedt/embeddium/impl/render/chunk/shader/ChunkShaderOptions;)Lorg/embeddedt/embeddium/impl/gl/shader/GlProgram;"))
 	private GlProgram<ChunkShaderInterface> redirectIrisProgram(ShaderChunkRenderer instance, ChunkShaderOptions options, TerrainRenderPass pass) {
@@ -34,5 +41,12 @@ public abstract class MixinShaderChunkRenderer {
 		}
 
 		return program;
+	}
+
+	@Inject(method = "end", at = @At("HEAD"))
+	private void end(TerrainRenderPass pass, CallbackInfo ci) {
+		if (this.activeProgram.getInterface() instanceof SodiumShader shader) {
+			shader.resetState();
+		}
 	}
 }
